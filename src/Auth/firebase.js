f38c05251c5f354e8f694ac2f97a648bcfb8e29e
AnishKr91620 <anish91620@gmail.com>
2025-02-09 08:42:12 +0530

    Aniket

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -3,13 +3,6 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 
 // 🔹 Replace with your Firebase Config
 const firebaseConfig = {
-    apiKey: "AIzaSyB9hgoGRTDObibru0Mf-E5W44FepxuXzrI",
-    authDomain: "asquare-1.firebaseapp.com",
-    projectId: "asquare-1",
-    storageBucket: "asquare-1.firebasestorage.app",
-    messagingSenderId: "882391488333",
-    appId: "1:882391488333:web:55e193d3bcd0ca598d8904",
-    measurementId: "G-3ZFWM4LFNJ"
 };
 
 const app = initializeApp(firebaseConfig);
